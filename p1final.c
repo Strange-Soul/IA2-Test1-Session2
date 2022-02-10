bab680c91cf3d7354c0d936cfc73a79aec00339d
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-10 13:46:19 +0000

    P1 Final.

---
+++ p1final.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int input_side()
+{
+ int n;
+ printf("Enter the Sides of Triangle \n");
+ scanf("%d",&n);
+ return n;
+}
+int check_scalene(int a,int b,int c)
+{
+ if(a!=b && b!=c && c!=a)
+ {
+   return 1;
+ }
+ else
+ return 0;
+}
+void output(int a,int b,int c,int isscalene)
+{
+ if(isscalene == 1)
+ {
+   printf("Triangle is Scalene \n");
+ }
+ else
+ {
+   printf("Triangle is not Scalene \n");
+ }
+}
+int main()
+{
+ int x,y,z,r;
+ x=input_side();
+ y=input_side();
+ z=input_side();
+ r=check_scalene(x,y,z);
+ output(x,y,z,r);
+ return 0;
+}
