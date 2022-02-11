3d10a1bc72914d7e89d0b9b363d0f30d2a94c9a3
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-11 13:04:48 +0000

    P3 Final

---
+++ p3final.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int input_number()
+{
+ int n;
+ printf("Enter any number \n");
+ scanf("%d",&n);
+ return n;
+}
+int is_composite(int n)
+{
+ int i;
+ for(i=2;i<n/2;i++)
+ {
+   if((n%i)==0)
+    return 1;
+ }
+ return 0;
+}
+void output(int n,int composite)
+{
+ if(composite)
+   printf("%d is  Composite Number\n",n);
+ else
+   printf("%d is not Composite Number \n",n);
+}
+int main()
+{
+ int x,y,z;
+ x=input_number();
+ y=is_composite(x);
+ output(x,y);
+ return 0;
+}
