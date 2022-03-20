c360e99a5b164ff2f5e56240ddf2d4eeb6fe1c23
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-20 08:17:05 +0000

    p0

--- p0final.c
+++ p0final.c
@@ -1,34 +1,14 @@
 #include<stdio.h>
-int input_number()
-{
-  int n;
-  printf("Enter a number to check Composite number or not !!! \n");
-  scanf("%d",&n);
-  return n;
-}
-int is_composite(int n)
-{
-  for(int i=2;i<n;i++)
-    {
-      if(n%i==0)
-        return 1;
-    }
-  return 0;
-}
-void output(int n,int composite)
-{
-  if(composite==1)
-  {
-    printf("%d is composite number \n",n);
-  }
-  else
-    printf("%d is not composite number \n",n);
-}
 int main()
 {
-  int n,result;
-  n=input_number();
-  result=is_composite(n);
-  output(result,n);
-  return 0;
+  char a[40];
+  printf("Enter the string \n");
+  scanf("%s",a);
+  for(int i=0;a[i]!='\0';i++)
+    {
+      if(a[i]>'a' && a[i]<'z')
+        a[i]=a[i]-32;
+    }
+  printf("Upper case of string is %s \n",a);
+ return 0;
 }
