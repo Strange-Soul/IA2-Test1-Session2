98162d4f3e99eca78ebd6ca254b6f494eccaba0c
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-24 08:56:00 +0000

    P8

--- p0final.c
+++ p0final.c
@@ -1,14 +1,72 @@
 #include<stdio.h>
+
+struct _triangle
+{
+ float base,height,area;
+};
+typedef struct _triangle Triangle;
+
+int input_n()
+{
+  int n;
+  printf("Enter the number of Triangle \n");
+  scanf("%d",&n);
+  return n;
+}
+Triangle input_triangle()
+{
+  Triangle t;
+  printf("Enter the base and height of Triangle \n");
+  scanf("%f %f",&t.base,&t.height);
+  return t;
+}
+void input_n_triangles(int n, Triangle t[n])
+{
+  for(int i=0;i<n;i++)
+    {
+      t[i]=input_triangle();
+    }
+}
+void find_area(Triangle *t)
+{
+  t->area=0.5*t->base*t->height;
+}
+void find_areas_n(int n, Triangle t[n])
+{
+  for(int i=0;i<n;i++)
+    {
+      find_area(&t[i]);
+    }
+}
+Triangle find_smallest_triangle(int n, Triangle t[n])
+{
+  Triangle small;
+  small=t[0];
+  for(int i=0;i<n;i++)
+    {
+      if(t[i].area<small.area)
+        small=t[i];
+    }
+  return small;
+}
+void output(int n, Triangle t[n], Triangle smallest)
+{
+  for(int i=0;i<n;i++)
+    {
+      printf("%f %f \n",t[i].base,t[i].height);
+    }
+  printf("%f %f \n",t[n-1].base,t[n-1].height);
+  printf("Samllest triangle is :: that having base %f and height of %f and its area=%f \n",smallest.base,smallest.height,smallest.area);
+}
 int main()
 {
-  char a[40];
-  printf("Enter the string \n");
-  scanf("%s",a);
-  for(int i=0;a[i]!='\0';i++)
-    {
-      if(a[i]>'a' && a[i]<'z')
-        a[i]=a[i]-32;
-    }
-  printf("Upper case of string is %s \n",a);
- return 0;
+  int n=input_n();
+  Triangle x,z;
+ // x=input_triangle();
+  Triangle p[n];
+  input_n_triangles(n,p);
+  find_areas_n(n,p);
+  z=find_smallest_triangle(n,p);
+  output(n,p,z);
+  return 0;
 }
--- p8final.c
+++ p8final.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 
 struct _triangle
 {
@@ -66,5 +66,80 @@ int main()
   find_areas_n(n,p);
   q=find_smallest_triangle(n,p);
   output(n,p,q);
+  return 0;
+}*/
+
+
+
+
+
+
+#include<stdio.h>
+
+struct _triangle
+{
+ float base,height,area;
+};
+typedef  struct _triangle Triangle;
+int input_n()
+{
+  int n;
+  printf("Enter the number of triangle \n");
+  scanf("%d",&n);
+  return n;
+}
+Triangle input_triangle()
+{
+  Triangle t;
+  printf("Enter the base and height of triangle \n");
+  scanf("%f %f",&t.base,&t.height);
+  return t;
+}
+void input_n_triangles(int n, Triangle t[n])
+{
+  printf("Enter %d triangles \n",n);
+  for(int i=0;i<n;i++)
+    scanf("%f %f",&t[i].base,&t[i].height);
+}
+void find_area(Triangle *t)
+{
+  t->area=0.5*(*t).base * (*t).height;
+}
+void find_areas_n(int n, Triangle t[n])
+{
+  for(int i=0;i<n;i++)
+    find_area(&t[i]);
+}
+Triangle find_smallest_triangle(int n, Triangle t[n])
+{
+  Triangle small;
+  small=t[0];
+  for(int i=0;i<n;i++)
+    {
+      if(small.area>=t[i].area)
+      small=t[i];
+    }
+  return small;
+}
+
+void output(int n, Triangle t[n], Triangle smallest)
+{
+  for(int i=0;i<n;i++)
+    {
+      printf(" %dth triangle  is having",i);
+      printf("Base=%f and Height=%f \n",t[i].base,t[i].height);
+    }
+  /*printf("%d th triangle is having ",n-1);
+  printf("Base=%f and height =%f \n",t[n-1].base,t[n-1].height);*/
+  printf("Smallest triangle is  with base=%f and height=%f having area=%f \n",smallest.base,smallest.height,smallest.area);
+}
+int main()
+{
+  int n=input_n();
+  Triangle p[n];
+  input_n_triangles(n,p);
+  find_areas_n(n,p);
+  Triangle result=find_smallest_triangle(n,p);
+  output(n,p,result);
   return 0;
 }
