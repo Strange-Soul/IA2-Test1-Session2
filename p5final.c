6e17c00fb344d8bf274f736f451d6b5af941192d
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-27 13:02:19 +0000

    P4

--- p5final.c
+++ p5final.c
@@ -1,47 +1,14 @@
 #include<stdio.h>
+#include<math.h>
+
 int input()
 {
- int n;
- printf("Enter any Number \n");
- scanf("%d",&n);
- return n;
+  int n;
+  printf("Enter the number \n");
+  scanf("%d",&n);
+  return n;
 }
 int gcd(int a,int b)
 {
- int large,small,i,rem;
- large=a>b?a:b;
- small=a<b?a:b;
- i=1;
- rem=1;
- while(rem)
- {
-   rem=large-i*small;
-   if(rem<small)
-   {
-     large=small;
-     small=rem;
-     i=0;
-   }
-   i++;
- }
- return large;
+  int large,small,i=1,rem=1
 }
-void output(int a,int b,int gcd)
-{
- printf("GCD of %d ,%d is: %d\n",a,b,gcd);
-}
-int main()
-{
- int x,y,z;
- x=input();
- y=input();
- z=gcd(x,y);
- output(x,y,z);
- return 0;
-}
-
-
-
-
-
-
