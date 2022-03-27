95629d961204d0f0a94a576921d2e4bf4c8c009c
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-27 12:18:19 +0000

    P2

--- p2final.c
+++ p2final.c
@@ -1,32 +1,35 @@
 #include<stdio.h>
 int input_side()
 {
- int n;
- printf("Enter the Side of  an Triangle \n");
- scanf("%d",&n);
- return n;
+  int n;
+  printf("Enter the sides of  Triangle \n");
+  scanf("%d",&n);
+  return n;
 }
 int check_scalene(int a,int b,int c)
 {
- if(a!=b && b!=c && c!=a)
- return 1;
- else
- return 0;
+  if(a==b && b==c && c==a)
+    return 1;
+  else
+    return 0;
 }
 void output(int a,int b,int c,int isscalene)
 {
- if(isscalene==1)
- printf("Triangle is Scalene \n");
- else
- printf("Triangle is not Scalene \n");
+  if(isscalene==1)
+  {
+    printf("Entered triangle is 'Equilateral triangle'!! \n");
+  }
+  else
+    printf("Entered triangle is 'Scalene triangle'!! \n");
 }
 int main()
 {
- int x,y,z,r;
- x=input_side();
- y=input_side();
- z=input_side();
- r=check_scalene(x,y,z);
- output(x,y,z,r);
- return 0;
+  int x,y,z;
+  x=input_side();
+  y=input_side();
+  z=input_side();
+  int result=check_scalene(x,y,z);
+  output(x,y,z,result);
+  return 0;
+  
 }
